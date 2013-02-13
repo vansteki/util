f4b66879a1fbd2f247b1d8d25171cb19df96db31
vansteki <vansteki.tw@gmail.com>
2013-02-14 02:27:08 +0800

    Update bakcup-script.php

--- bakcup-script.php
+++ bakcup-script.php
@@ -7,17 +7,17 @@ $path = $base.$today;
 $table_full = array("table1", "table2"); //backup tables that include data
 $table_schema = array("table3", "table4"); //only backup schema
 
-  //backup to home
-	$i =0;
-	while(isset($table_full[$i])){
-	    system("mysqldump -u user -p password $db $table_full[$i] > $path/$table_full[$i].sql");
-	    $i++;
-	}
+//backup to home
+$i = 0;
+while(isset($table_full[$i])){
+	system("mysqldump -u user -p password $db $table_full[$i] > $path/$table_full[$i].sql");
+	$i++;
+}
 
-	// only backup table schema
-	$j = 0;
-	while(isset($table_schema[$j])){
-	    system("mysqldump -u user -p password -d $db $table_schema[$j] > $path/$table_schema[$j].sql");
-	    $j++;
-	}
+// only backup table schema
+$j = 0;
+while(isset($table_schema[$j])){
+	system("mysqldump -u user -p password -d $db $table_schema[$j] > $path/$table_schema[$j].sql");
+	$j++;
+}
 ?>
