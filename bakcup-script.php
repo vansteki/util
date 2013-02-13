24484ae3be19b5f3b072fec980d568a85a600059
vansteki <vansteki.tw@gmail.com>
2013-02-13 10:25:12 -0800

    Create bakcup-script.php
    it's for mysql.

---
+++ bakcup-script.php
@@ -0,0 +1,23 @@
+<?php
+$db = '';
+$base = "/path/of/backup/folder/";
+$today = date("Ymd"); //Ymd_his
+system("mkdir $base$today"); //create folder named by today
+$path = $base.$today;
+$table_full = array("table1", "table2"); //backup tables that include data
+$table_schema = array("table3", "table4"); //only backup schema
+
+  //backup to home
+	$i =0;
+	while(isset($table_full[$i])){
+	    system("mysqldump -u user -p password $db $table_full[$i] > $path/$table_full[$i].sql");
+	    $i++;
+	}
+
+	// only backup table schema
+	$j = 0;
+	while(isset($table_schema[$j])){
+	    system("mysqldump -u user -p password -d $db $table_schema[$j] > $path/$table_schema[$j].sql");
+	    $j++;
+	}
+?>
