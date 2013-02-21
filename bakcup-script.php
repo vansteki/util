8d2b1b3954387d27f27300b7b97fdfb26c68e6ea
vansteki <vansteki.tw@gmail.com>
2013-02-21 17:18:44 +0800

    Update bakcup-script.php

--- bakcup-script.php
+++ bakcup-script.php
@@ -1,5 +1,7 @@
 <?php
 $db = '';
+$db_user = '';
+$db_pass = '';
 $base = "/path/of/backup/folder/";
 $today = date("Ymd"); //Ymd_his
 system("mkdir $base$today"); //create folder named by today
@@ -10,14 +12,14 @@ $table_schema = array("table3", "table4"); //only backup schema
 //backup to home
 $i = 0;
 while(isset($table_full[$i])){
-	system("mysqldump -u user -p password $db $table_full[$i] > $path/$table_full[$i].sql");
+	system("mysqldump -u $db_user -p$db_pass $db $table_full[$i] > $path/$table_full[$i].sql");
 	$i++;
 }
 
 // only backup table schema
 $j = 0;
 while(isset($table_schema[$j])){
-	system("mysqldump -u user -p password -d $db $table_schema[$j] > $path/$table_schema[$j].sql");
+	system("mysqldump -u $db_user -p$db_pass -d $db $table_schema[$j] > $path/$table_schema[$j].sql");
 	$j++;
 }
 ?>
